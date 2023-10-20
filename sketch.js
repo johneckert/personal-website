b00a288795c147511fbfbc2c1c7bf3b004d02e0e
John Eckert <johnteckert@gmail.com>
2023-10-20 10:20:30 -0400

    refactor and cleanup

--- sketch.js
+++ sketch.js
@@ -1,10 +1,10 @@
-let blobs = []
+let blobs = [];
+let svg, blobGradient;
 
 function setup() {
   createCanvas(window.innerWidth, window.innerHeight);
-  colorMode(HSL)
   
-  svg = SVG().addTo('#lava-lamp').size(window.innerWidth, window.innerHeight)
+  svg = SVG().addTo('#lava-lamp').size(window.innerWidth, window.innerHeight);
   
   blobGradient = svg.gradient('radial', function(add) {
     add.stop(0, '#FFFFFF')
@@ -12,38 +12,35 @@ function setup() {
     add.stop(0.4, '#a12bc4')
     add.stop(0.8, '#772091')
     add.stop(1, '#2b0a3d')
-  })
+  });
   
-  let group = makeFilter()
+  let group = makeFilter();
 
   for (let i = 0; i < Math.ceil(window.innerHeight / 20); i++) {
-    blobs.push(new Goop(group))
+    blobs.push(new Goop(group));
   }
-  
-  group.fill('#ff9f60')
 }
-
 function makeFilter() {
-  let filter = svg.element('filter').attr('id', 'goo')
+  let filter = svg.element('filter').attr('id', 'goo');
   
   filter.add(svg.element('feGaussianBlur').attr({
     in: "SourceGraphic", 
     stdDeviation: 10,
     result: "background-blur",
-  }))
+  }));
 
   filter.add(svg.element('feColorMatrix').attr({
     in: "background-blur",
     type: "matrix",
     values: "1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 20 -5",
     result: "goo",
-  }))
+  }));
   
-  return svg.group().attr('filter', 'url(#goo)')
+  return svg.group().attr('filter', 'url(#goo)');
 }
 
 function draw() {
-  for (let blob of blobs) blob.update()
+  for (let blob of blobs) blob.update();
 }
 
 class Goop {
@@ -62,21 +59,21 @@ class Goop {
   }
   
   update() {
-    let scale = 0.2
-    let sizeX = (1 + sin(this.wobble) * scale) * this.initialSize
-    let sizeY = (1 + sin(this.wobble + PI) * scale) * this.initialSize
+    let scale = 0.2;
+    let sizeX = (1 + sin(this.wobble) * scale) * this.initialSize;
+    let sizeY = (1 + sin(this.wobble + PI) * scale) * this.initialSize;
     
-    let acceleration = createVector(0, map(this.position.y, 0, window.innerHeight, 1, -1) * 0.1 / this.initialSize)
-    this.velocity.add(acceleration)
-    this.velocity.limit(0.5)
-    this.position.add(this.velocity)
+    let acceleration = createVector(0, map(this.position.y, 0, window.innerHeight, 1, -1) * 0.1 / this.initialSize);
+    this.velocity.add(acceleration);
+    this.velocity.limit(0.5);
+    this.position.add(this.velocity);
     
     // update wobble based on magnitude of velocity
-    this.wobble += abs(this.velocity.mag()) * 0.05 + 0.005
+    this.wobble += abs(this.velocity.mag()) * 0.05 + 0.005;
     
     this.element
       .size(sizeX, sizeY)
-      .center(this.position.x, this.position.y)
+      .center(this.position.x, this.position.y);
   }
 }
 
