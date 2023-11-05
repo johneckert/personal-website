7b19e3608382aeb9a517bafc0b63fa6a00e5d3c0
John Eckert <johnteckert@gmail.com>
2023-11-05 10:53:36 -0500

    clean up

--- sketch.js
+++ sketch.js
@@ -18,7 +18,6 @@ function setup() {
   createCanvas(width, height);
   frameRate(32);
 
-  // Create gradient
   backgroundGradient = drawingContext.createLinearGradient(0, 0, width, height);
   backgroundGradient.addColorStop(0.3, color(55, 213, 214))
   backgroundGradient.addColorStop(0.8, color(0,151,151))
@@ -32,6 +31,9 @@ function draw() {
   background(50, 89, 100);
   drawingContext.fillStyle = backgroundGradient;
   drawingContext.fillRect(0, 0, width, height);
+  // drawingContext.filter = 'blur(0.5px)';
+  // drawingContext.filter = 'drop-shadow(5px 5px 5px rgba(0, 0, 0, 0.2))'
+
   for (let i = 0; i < blobs.length; i++) {
     let gravity = createVector(0, 0.1 * blobs[i].mass);
     blobs[i].applyForce(gravity);
@@ -54,53 +56,49 @@ class Blob {
     this.position = createVector(random(width), random(height));
     this.velocity = createVector(0, 0);
     this.acceleration = createVector(0, 0);
-    this.gravityCenter = createVector(this.position.x, this.position.y * -1); // Center of gravity
+    this.centerOfGravity = createVector(this.position.x, this.position.y * -1);
 
     this.updateGradient();
   }
 
   updateGradient() {
-    this.gradient = drawingContext.createRadialGradient(this.position.x -40, this.position.y- 30, 0, this.position.x, this.position.y, this.diameter);
+    this.gradient = drawingContext.createRadialGradient(this.position.x - 30, this.position.y - 20, 0, this.position.x, this.position.y, this.diameter);
     this.gradient.addColorStop(0, color('#c536F0'))
     this.gradient.addColorStop(0.2, color('#a12bc4'))
     this.gradient.addColorStop(0.4, color('#772091'))
-    this.gradient.addColorStop(0.8, color('#2b0a3d'))
+    this.gradient.addColorStop(0.9, color('#2b0a3d'))
   }
-
-
-
-  // Newton's 2nd law: F = M * A
   applyForce(force) {
+    // Newton's 2nd law: F = M * A
     let f = force.copy();
     f.div(this.mass);
     this.acceleration.add(f);
   }
-  // Reverse direction of acceleration if blob hits the edge of the canvas
   bounce() {
+    // Reverse direction of acceleration if blob hits the edge of the canvas
     if (this.position.y > height - this.mass * 8) {
-      this.gravityCenter = createVector(this.position.x, 0);
+      this.centerOfGravity = createVector(this.position.x, 0);
     }
 
     if (this.position.y < this.mass * 8) {
-      this.gravityCenter = createVector(this.position.x, height);
+      this.centerOfGravity = createVector(this.position.x, height);
     }
   };
 
   wibble() {
-    // this.diameter += sin(this.wobble);
     this.diameterX *= (1 + sin(this.wobble) * 0.005);
     this.diameterY *= (1 + sin(this.wobble + PI) * 0.005);
     this.wobble += abs(this.velocity.mag()) * 0.05 + 0.005;
   }
 
   move() {
-    this.gravityForce = p5.Vector.sub(this.gravityCenter, this.position);
+    this.gravityForce = p5.Vector.sub(this.centerOfGravity, this.position);
     this.acceleration.add(this.gravityForce);
     this.velocity.add(this.acceleration); // Velocity changes according to acceleration
     this.velocity.limit(0.5);
     this.position.add(this.velocity);
 
-    // We must clear acceleration each frame
+    // clear acceleration each frame
     this.acceleration.mult(0);
   }
 
